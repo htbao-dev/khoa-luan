29ce61f93001739628eb4d0c5ce5f24ccc38ff85
BaoHuynh <ht.bao1112000@gmail.com>
2022-01-23 11:39:38 +0700

    connect to cloudDB

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try{
-        await mongoose.connect('mongodb://localhost:27017/social_network_dev');
+        await mongoose.connect('mongodb+srv://18T1021011:18T1021011@khoaluan.pesbl.mongodb.net/social_network_deploy?retryWrites=true&w=majority');
         console.log('Connected to mongodb');
     }
     catch(error){
